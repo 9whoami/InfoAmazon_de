8bbee93f5567372c5907772ebfa6cdd45697d15a
whoami <whoami@expressmail.dk>
2016-02-03 22:07:11 +0500

    альфа версия. без сохранения информации

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+import re
 from os import sys
+from lxml.html import tostring
 from grab import Grab
 import settings
 
@@ -25,6 +27,40 @@ def main(*args):
         if 'keywords=' in link:
             print(link)
             print('\n')
+
+            # переходим по линке на итем
+            g = Grab()
+            g.go(link)
+            try:
+                seller_link = g.doc.tree.xpath(".//*[@id='merchant-info']/a")[0].get('href')
+            except IndexError:
+                print("Item", link, ": Seller not found.")
+                continue
+
+            seller_id = re.findall(settings.PATTERN, seller_link)[0]
+            # переходим на страницу с подробными данными по селлеру
+            g.go(settings.SELLER_URL.format(seller_id))
+
+            # seller_info = g.doc.tree.xpath(".//*[@id='aag_detailsAbout']")[0]
+            # print(tostring(seller_info, encoding='utf-8'))
+
+            name = ".//*[@id='aag_detailsAbout']/h3"
+            register_name = ".//*[@id='aag_detailsAbout']/p/strong/span[2]"
+            adress = ".//*[@id='aag_detailsAbout']/p/strong/span[3]"
+            telephone = ".//*[@id='aag_detailsAbout']/p/strong/span[4]"
+            telephaks = ".//*[@id='aag_detailsAbout']/p/strong/span[5]"
+            email = ".//*[@id='aag_detailsAbout']/p/strong/span[7]"
+            register_data = ".//*[@id='aag_detailsAbout']/p/strong/span[9]"
+
+            xpaths = [name, register_name, adress, telephone, telephaks, email, register_data]
+
+            for xpath in xpaths:
+                data = g.tree.xpath(xpath)[0].text
+                print(data)
+            # емайл закодили совсем по тупому... но это решаемо
+            email = g.tree.xpath(xpaths[5])[0]
+            print(tostring(email, encoding='utf-8'))
+
     del grab
 
 
--- settings/base.py
+++ settings/base.py
@@ -3,6 +3,8 @@
 # Урл для поиска товаров. Вместо '{}' будут подставлены поисковые выражения.
 SEARCH_URL = "http://www.amazon.de/s/ref=nb_sb_noss?__mk_de_DE=%C3%85M%C3%85%C5%BD%C3%95%C3%91&url=node%3D1981733031&field-keywords={}"
 
+SELLER_URL = "http://www.amazon.de/gp/aag/details/ref=aag_m_as?ie=UTF8&asin=&isAmazonFulfilled=&isCBA=&seller={}"
+
 # Поисковые выражения
 PHRASES = (
     'Bra damen',
@@ -17,3 +19,5 @@ XPATH = dict(
     items='.//*/a[@class="a-link-normal s-access-detail-page  a-text-normal"]',
 
 )
+
+PATTERN = '&seller=(.+)'
