22ab7afa466c66e52d5702acc7f1fa935f06acc8
whoami <whoami@expressmail.dk>
2016-02-03 18:24:18 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+from os import sys
+
+__author__ = 'whoami'
+__version__ = '0.0.0'
+__date__ = '03.02.16 18:43'
+__description__ = """
+Description for the python module
+"""
+
+
+def main(*args):
+    print(args)
+
+
+if __name__ in "__main__":
+    main(sys.argv)
