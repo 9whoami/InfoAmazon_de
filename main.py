e3befd6c2100db432e4f87de56cfdef89d0b6728
whoami <whoami@expressmail.dk>
2016-02-04 15:01:23 +0500

    пробная версия.

--- main.py
+++ main.py
@@ -1,68 +1,115 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import re
-from os import sys
+import csv
 from lxml.html import tostring
 from grab import Grab
+from grab.error import GrabTimeoutError
 import settings
 
 __author__ = 'whoami'
-__version__ = '0.0.0'
+__version__ = '0.1.0'
 __date__ = '03.02.16 18:43'
 __description__ = """
 Парсим amazon. Получаем информацию о продавцах и пишем ее в csv файл.
 """
 
 
-def main(*args):
+def save_info(data):
+    with open("some.csv", "a") as csv_fh:
+        writer = csv.writer(csv_fh, delimiter=':', quoting=csv.QUOTE_MINIMAL, quotechar='`', lineterminator='|')
+        writer.writerows([
+                          data
+                        ])
+
+
+# def save_info():
+#     output_file = open("result.csv", "w")
+#     wrtr = csv.DictWriter(output_file, fieldnames=['name', 'number', 'text'])
+#     fieldname = dict(name='name', number="number", text='text')
+#     rec = dict(name='1', number='2', text='3')
+#     wrtr.writerow(fieldname)
+#     wrtr.writerow(rec)
+#     output_file.close()
+
+
+def main():
     # выводим высе найденые товары
-    print(args)
     print("Start working")
     grab = Grab()
     grab.go(settings.SEARCH_URL.format(settings.PHRASES[0]))
-    items = grab.doc.tree.xpath(settings.XPATH['items'])
-    for item in items:
-        link = item.get('href')
-        # if`ом исключаем мусор. иногда попадается 3 лишних результата
-        if 'keywords=' in link:
-            print(link)
-            print('\n')
-
+    for i in range(2, 400):
+        print("Page:", i)
+        items = grab.doc.tree.xpath(settings.XPATH['items'])
+        for j, item in enumerate(items):
+            link = item.get('href')
+            # if`ом исключаем мусор. иногда попадается 3 лишних результата
+            if 'keywords=' not in link:
+                continue
             # переходим по линке на итем
             g = Grab()
-            g.go(link)
+            try:
+                g.go(link)
+            except GrabTimeoutError as e:
+                print("При загрузке", link, "возбуждено исключение", e)
+                continue
             try:
                 seller_link = g.doc.tree.xpath(".//*[@id='merchant-info']/a")[0].get('href')
             except IndexError:
-                print("Item", link, ": Seller not found.")
                 continue
 
-            seller_id = re.findall(settings.PATTERN, seller_link)[0]
+            seller_id = re.findall(settings.PATTERNS['seller_id'],
+                                   seller_link)[0]
             # переходим на страницу с подробными данными по селлеру
-            g.go(settings.SELLER_URL.format(seller_id))
+            seller_link = settings.SELLER_URL.format(seller_id)
+            try:
+                g.go(seller_link)
+            except GrabTimeoutError as e:
+                print("При загрузке", seller_link, "возбуждено исключение", e)
+                continue
 
-            # seller_info = g.doc.tree.xpath(".//*[@id='aag_detailsAbout']")[0]
-            # print(tostring(seller_info, encoding='utf-8'))
+            try:
+                seller_full_info = g.doc.tree.xpath(".//*[@id='aag_detailsAbout']")[0]
+            except IndexError:
+                print(link)
+                print("Seler info not found", settings.SELLER_URL.format(seller_id))
+                continue
 
-            name = ".//*[@id='aag_detailsAbout']/h3"
-            register_name = ".//*[@id='aag_detailsAbout']/p/strong/span[2]"
-            adress = ".//*[@id='aag_detailsAbout']/p/strong/span[3]"
-            telephone = ".//*[@id='aag_detailsAbout']/p/strong/span[4]"
-            telephaks = ".//*[@id='aag_detailsAbout']/p/strong/span[5]"
-            email = ".//*[@id='aag_detailsAbout']/p/strong/span[7]"
-            register_data = ".//*[@id='aag_detailsAbout']/p/strong/span[9]"
+            seller_full_info_html = tostring(seller_full_info, encoding='utf-8').decode('utf-8')
+            seller_full_info = re.sub(r'\<[^>]*\>', '', seller_full_info_html)
+            seller_full_info = re.sub(r'(?<=\s)\s', '', seller_full_info)
+            seller_info = [
+                re.findall(settings.PATTERNS['name'], seller_full_info_html,
+                           re.MULTILINE),
+                re.findall(settings.PATTERNS['email'], seller_full_info_html,
+                           re.MULTILINE),
+                re.findall(settings.PATTERNS['phone'], seller_full_info_html,
+                           re.MULTILINE),
+                re.findall(settings.PATTERNS['fax'], seller_full_info_html,
+                           re.MULTILINE),
+                re.findall(settings.PATTERNS['site'], seller_full_info_html,
+                           re.MULTILINE),
+                seller_link,
+                seller_full_info,]
 
-            xpaths = [name, register_name, adress, telephone, telephaks, email, register_data]
+            save_info(seller_info)
 
-            for xpath in xpaths:
-                data = g.tree.xpath(xpath)[0].text
-                print(data)
-            # емайл закодили совсем по тупому... но это решаемо
-            email = g.tree.xpath(xpaths[5])[0]
-            print(tostring(email, encoding='utf-8'))
+            # with open(seller_info[0] + '.txt', 'w') as f:
+            #     f.write(link + '\n')
+            #     f.write(seller_link + '\n')
+            #     f.write(seller_full_info)
+            #     f.write(str(seller_info))
+            del g
+
+        next_link = 'https://www.amazon.de'
+        next_link += grab.doc.tree.xpath(".//*[@id='pagnNextLink']")[0].get('href')
+        grab.go(
+            next_link
+        )
 
     del grab
 
 
 if __name__ in "__main__":
-    main(sys.argv)
+    # save_info()
+    main()
--- settings/base.py
+++ settings/base.py
@@ -3,6 +3,7 @@
 # Урл для поиска товаров. Вместо '{}' будут подставлены поисковые выражения.
 SEARCH_URL = "http://www.amazon.de/s/ref=nb_sb_noss?__mk_de_DE=%C3%85M%C3%85%C5%BD%C3%95%C3%91&url=node%3D1981733031&field-keywords={}"
 
+#
 SELLER_URL = "http://www.amazon.de/gp/aag/details/ref=aag_m_as?ie=UTF8&asin=&isAmazonFulfilled=&isCBA=&seller={}"
 
 # Поисковые выражения
@@ -15,9 +16,18 @@ PROXY_LIST = (
 
 )
 
+#
 XPATH = dict(
     items='.//*/a[@class="a-link-normal s-access-detail-page  a-text-normal"]',
 
 )
 
-PATTERN = '&seller=(.+)'
+#
+PATTERNS = dict(
+    seller_id=r'&seller\=(.+)',
+    name=r'\<h3\>(.+?)\s\<\/h3\>',
+    phone=r'(Telefon|Tel|Phone):\s*([0-9\-\s\(\)\/\+]+)\<',
+    fax=r'(Telefax|Fax):\s([0-9\-\s\(\)\/\+]+)\<',
+    site=r'www\.(.+?)\<',
+    email=r'(Email|E-Mail|eMail):.+?([A-z0-9\_\-\.]+@[A-z0-9\.\-]+\.[A-z0-9]+)\<'
+)
